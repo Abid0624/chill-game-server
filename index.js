49ab1b9a9093ded1939cc6a07e29ba297d6d207f
Abid064 <abidkhan09989@gmail.com>
2025-05-06 03:54:00 +0600

    users apis added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,6 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const gameCollection = client.db("gameDB").collection("game");
+    const userCollection = client.db("gameDB").collection("users");
 
     app.get("/game", async (req, res) => {
       const cursor = gameCollection.find();
@@ -34,10 +35,36 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/game/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await gameCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/game", async (req, res) => {
       const newGame = req.body;
       console.log(newGame);
       const result = await gameCollection.insertOne(newGame);
+      res.send(result);
+    });
+
+    // users related apis
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.patch("/users", async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
   } finally {
